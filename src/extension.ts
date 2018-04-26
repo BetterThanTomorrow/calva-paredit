94e29282c8f98068008365ffcdb5e37cabda3e7e
Peter Strömberg <pez@pezius.com>
2018-04-26 21:55:16 +0200

    Some minor cleanup

--- src/extension.ts
+++ src/extension.ts
@@ -1,81 +1,86 @@
 'use strict';
-import {StatusBar} from './status_bar';
+import { StatusBar } from './status_bar';
 import * as utils from './utils';
-import {commands, window, ExtensionContext} from 'vscode';
+import { commands, window, ExtensionContext } from 'vscode';
 let paredit = require('paredit.js');
 
 const languages = new Set(["clojure", "lisp", "scheme"]);
 let enabled = true;
 
 const navigate = (fn, ...args) =>
-    ({textEditor, ast, selection}) => {
+    ({ textEditor, ast, selection }) => {
         let res = fn(ast, selection.cursor, ...args);
         utils.select(textEditor, res);
     }
 const navigateRange = (fn, ...args) =>
-    ({textEditor, ast, selection}) => {
+    ({ textEditor, ast, selection }) => {
         let res = fn(ast, selection.start, selection.end, ...args);
         utils.select(textEditor, res);
     }
 
-function indent({textEditor, range}) {
+function indent({ textEditor, range }) {
     let src = textEditor.document.getText(),
         ast = paredit.parse(src),
         res = paredit.editor.indentRange(ast, src, range.start, range.end);
 
     utils
-    .edit(textEditor, utils.commands(res))
-    .then((applied?) => utils.undoStop(textEditor));
+        .edit(textEditor, utils.commands(res))
+        .then((applied?) => utils.undoStop(textEditor));
 }
 
 const edit = (fn, ...args) =>
-    ({textEditor, src, ast, selection}) => {
+    ({ textEditor, src, ast, selection }) => {
         let res = fn(ast, src, selection.cursor, ...args);
 
         if (res)
             if (res.changes.length > 0) {
                 let cmd = utils.commands(res),
-                    sel = { start: Math.min(...cmd.map(c => c.start)),
-                            end:   Math.max(...cmd.map(utils.end)) };
+                    sel = {
+                        start: Math.min(...cmd.map(c => c.start)),
+                        end: Math.max(...cmd.map(utils.end))
+                    };
 
                 utils
-                .edit(textEditor, cmd)
-                .then((applied?) => {
-                    utils.select(textEditor, res.newIndex);
-                    indent({ textEditor: textEditor,
-                             range:      sel })});
+                    .edit(textEditor, cmd)
+                    .then((applied?) => {
+                        utils.select(textEditor, res.newIndex);
+                        indent({
+                            textEditor: textEditor,
+                            range: sel
+                        })
+                    });
             }
             else
                 utils.select(textEditor, res.newIndex);
     }
 
-const pareditCommands : [[string, Function]] = [
+const pareditCommands: [[string, Function]] = [
 
     // NAVIGATION
-    ['paredit.forwardSexp',            navigate(paredit.navigator.forwardSexp)],
-    ['paredit.backwardSexp',           navigate(paredit.navigator.backwardSexp)],
-    ['paredit.forwardDownSexp',        navigate(paredit.navigator.forwardDownSexp)],
-    ['paredit.backwardUpSexp',         navigate(paredit.navigator.backwardUpSexp)],
-    ['paredit.sexpRangeExpansion',     navigateRange(paredit.navigator.sexpRangeExpansion)],
-    ['paredit.closeList',              navigate(paredit.navigator.closeList)],
-    ['paredit.rangeForDefun',          navigate(paredit.navigator.rangeForDefun)],
-        
+    ['paredit.forwardSexp', navigate(paredit.navigator.forwardSexp)],
+    ['paredit.backwardSexp', navigate(paredit.navigator.backwardSexp)],
+    ['paredit.forwardDownSexp', navigate(paredit.navigator.forwardDownSexp)],
+    ['paredit.backwardUpSexp', navigate(paredit.navigator.backwardUpSexp)],
+    ['paredit.sexpRangeExpansion', navigateRange(paredit.navigator.sexpRangeExpansion)],
+    ['paredit.closeList', navigate(paredit.navigator.closeList)],
+    ['paredit.rangeForDefun', navigate(paredit.navigator.rangeForDefun)],
+
     // EDITING
-    ['paredit.slurpSexpForward',       edit(paredit.editor.slurpSexp, {'backward': false})],
-    ['paredit.slurpSexpBackward',      edit(paredit.editor.slurpSexp, {'backward': true})],
-    ['paredit.barfSexpForward',        edit(paredit.editor.barfSexp, {'backward': false})],
-    ['paredit.barfSexpBackward',       edit(paredit.editor.barfSexp, {'backward': true})],
-    ['paredit.spliceSexp',             edit(paredit.editor.spliceSexp)],
-    ['paredit.splitSexp',              edit(paredit.editor.splitSexp)],
-    ['paredit.killSexpForward',        edit(paredit.editor.killSexp, {'backward': false})],
-    ['paredit.killSexpBackward',       edit(paredit.editor.killSexp, {'backward': true})],
-    ['paredit.spliceSexpKillForward',  edit(paredit.editor.spliceSexpKill, {'backward': false})],
-    ['paredit.spliceSexpKillBackward', edit(paredit.editor.spliceSexpKill, {'backward': true})],
-    ['paredit.wrapAroundParens',       edit(paredit.editor.wrapAround, '(', ')')],
-    ['paredit.wrapAroundSquare',       edit(paredit.editor.wrapAround, '[', ']')],
-    ['paredit.wrapAroundCurly',        edit(paredit.editor.wrapAround, '{', '}')],
-    ['paredit.indentRange',            indent],
-    ['paredit.transpose',              edit(paredit.editor.transpose)]];
+    ['paredit.slurpSexpForward', edit(paredit.editor.slurpSexp, { 'backward': false })],
+    ['paredit.slurpSexpBackward', edit(paredit.editor.slurpSexp, { 'backward': true })],
+    ['paredit.barfSexpForward', edit(paredit.editor.barfSexp, { 'backward': false })],
+    ['paredit.barfSexpBackward', edit(paredit.editor.barfSexp, { 'backward': true })],
+    ['paredit.spliceSexp', edit(paredit.editor.spliceSexp)],
+    ['paredit.splitSexp', edit(paredit.editor.splitSexp)],
+    ['paredit.killSexpForward', edit(paredit.editor.killSexp, { 'backward': false })],
+    ['paredit.killSexpBackward', edit(paredit.editor.killSexp, { 'backward': true })],
+    ['paredit.spliceSexpKillForward', edit(paredit.editor.spliceSexpKill, { 'backward': false })],
+    ['paredit.spliceSexpKillBackward', edit(paredit.editor.spliceSexpKill, { 'backward': true })],
+    ['paredit.wrapAroundParens', edit(paredit.editor.wrapAround, '(', ')')],
+    ['paredit.wrapAroundSquare', edit(paredit.editor.wrapAround, '[', ']')],
+    ['paredit.wrapAroundCurly', edit(paredit.editor.wrapAround, '{', '}')],
+    ['paredit.indentRange', indent],
+    ['paredit.transpose', edit(paredit.editor.transpose)]];
 
 function wrapPareditCommand(fn) {
     return () => {
@@ -85,10 +90,12 @@ function wrapPareditCommand(fn) {
         if (!enabled || !languages.has(doc.languageId)) return;
 
         let src = textEditor.document.getText();
-        fn({ textEditor: textEditor,
-             src:        src,
-             ast:        paredit.parse(src),
-             selection:  utils.getSelection(textEditor) });
+        fn({
+            textEditor: textEditor,
+            src: src,
+            ast: paredit.parse(src),
+            selection: utils.getSelection(textEditor)
+        });
     }
 }
 
@@ -99,11 +106,11 @@ export function activate(context: ExtensionContext) {
     context.subscriptions.push(
 
         statusBar,
-        commands.registerCommand('paredit.toggle', () => {enabled = !enabled; statusBar.enabled = enabled;}),
+        commands.registerCommand('paredit.toggle', () => { enabled = !enabled; statusBar.enabled = enabled; }),
         window.onDidChangeActiveTextEditor((e) => statusBar.visible = languages.has(e.document.languageId)),
 
         ...pareditCommands
-           .map(([command, fn]) => commands.registerCommand(command, wrapPareditCommand(fn))));
+            .map(([command, fn]) => commands.registerCommand(command, wrapPareditCommand(fn))));
 }
 
 export function deactivate() {
--- src/status_bar.ts
+++ src/status_bar.ts
@@ -1,31 +1,25 @@
 'use strict';
-import {window, StatusBarAlignment, StatusBarItem} from 'vscode';
+import { window, StatusBarAlignment, StatusBarItem } from 'vscode';
 
 const activeColour = "white";
 const inactiveColour = "#b3b3b3";
 
-const colour = {"active": "white", "inactive": "#b3b3b3"};
+const colour = { "active": "white", "inactive": "#b3b3b3" };
 
 export class StatusBar {
 
     private _enabled: Boolean;
-    private _strict: Boolean;
     private _visible: Boolean;
 
     private _toggleBarItem: StatusBarItem;
-    private _strictBarItem: StatusBarItem;
 
     constructor(enabled = true, strict = true, visible = true) {
-        this._toggleBarItem = window.createStatusBarItem();
+        this._toggleBarItem = window.createStatusBarItem(StatusBarAlignment.Right);
         this._toggleBarItem.text = "(λ)";
         this._toggleBarItem.command = 'paredit.toggle';
         this.enabled = enabled;
         this.visible = visible;
 
-        this._strictBarItem = window.createStatusBarItem();
-        this._strictBarItem.text = "strict";
-        this._strictBarItem.command = 'paredit.toggleStrict';
-        this.strict = strict;
         this.visible = visible;
     }
 
@@ -41,23 +35,7 @@ export class StatusBar {
             this._toggleBarItem.color = colour.active;
         } else {
             this._toggleBarItem.tooltip = "Enable Paredit"
-            this._toggleBarItem.color = colour.inactive;  
-        }
-    }
-
-    get strict() {
-        return this._strict;
-    }
-
-    set strict(value: Boolean) {
-        this._strict = value;
-
-        if (this._strict) {
-            this._strictBarItem.tooltip = "Disable Paredit Strict"
-            this._strictBarItem.color = colour.active;
-        } else {
-            this._strictBarItem.tooltip = "Enable Paredit Strict"
-            this._strictBarItem.color = colour.inactive;  
+            this._toggleBarItem.color = colour.inactive;
         }
     }
 
@@ -68,15 +46,12 @@ export class StatusBar {
     set visible(value: Boolean) {
         if (value) {
             this._toggleBarItem.show();
-            //this._strictBarItem.show();
         } else {
             this._toggleBarItem.hide();
-            //this._strictBarItem.hide();
         }
     }
 
     dispose() {
         this._toggleBarItem.dispose();
-        this._strictBarItem.dispose();
     }
 }
